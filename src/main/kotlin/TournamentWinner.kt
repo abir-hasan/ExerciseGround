0e39cd50075be088e44072ed5d1ae298ed4270ee
Abir Hasan <abirhasan89@gmail.com>
2021-09-30 14:07:23 +0600

    Problem: TournamentWinner - v2

--- src/main/kotlin/TournamentWinner.kt
+++ src/main/kotlin/TournamentWinner.kt
@@ -7,33 +7,43 @@
 fun tournamentWinner(competitions: List<List<String>>, results: List<Int>): String {
     val map = HashMap<String, Int>()
 
+    var maxValue = -1
+    var winner = ""
+
     for ((i, competition) in competitions.withIndex()) {
         val comResult = results[i]
         if (comResult == 0) {
             val awayTeam: String = competition[1]
             if (map.containsKey(awayTeam)) {
-                val score: Int = map[awayTeam]!!
-                map[awayTeam] = score + 3
+                val score: Int = map[awayTeam]!! + 3
+                map[awayTeam] = score
+                if (score > maxValue) {
+                    winner = awayTeam
+                    maxValue = score
+                }
             } else {
                 map[awayTeam] = 3
+                if (3 > maxValue) {
+                    winner = awayTeam
+                    maxValue = 3
+                }
             }
         } else {
             val homeTeam: String = competition[0]
             if (map.containsKey(homeTeam)) {
-                val score: Int = map[homeTeam]!!
-                map[homeTeam] = score + 3
+                val score: Int = map[homeTeam]!! + 3
+                map[homeTeam] = score
+                if (score > maxValue) {
+                    winner = homeTeam
+                    maxValue = score
+                }
             } else {
                 map[homeTeam] = 3
+                if (3 > maxValue) {
+                    winner = homeTeam
+                    maxValue = 3
+                }
             }
-        }
-    }
-
-    var maxValue = -1
-    var winner = ""
-    for ((teamName, score) in map.entries) {
-        if (score > maxValue) {
-            winner = teamName
-            maxValue = score
         }
     }
     return winner
