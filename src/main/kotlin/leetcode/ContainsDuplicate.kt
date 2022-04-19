e71f8bb46c3486a460d9594a5ac63dbc6d9a6b14
Abir Hasan <abirhasan89@gmail.com>
2022-04-19 11:52:48 +0600

    LeetCode - array - contains duplicate - easy

---
+++ src/main/kotlin/leetcode/ContainsDuplicate.kt
@@ -0,0 +1,25 @@
+package leetcode
+
+/**
+ * Created By Abir Hasan on 19-April-22
+ * Problem: Contains Duplicate
+ * Category: Arrays
+ * Link:
+ */
+
+fun containsDuplicate(nums: IntArray): Boolean {
+    val itemSet = mutableSetOf<Int>()
+    nums.forEach {
+        if (itemSet.contains(it))
+            return true
+        else
+            itemSet.add(it)
+    }
+    return false
+}
+
+fun main() {
+    //val result = containsDuplicate(intArrayOf(1,1,1,3,3,4,3,2,4,2))
+    val result = containsDuplicate(intArrayOf(1, 2, 3, 4))
+    println(result)
+}
