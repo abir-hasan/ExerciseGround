4b724c63546cf73365b6bc84e887cac491e54056
Abir Hasan <abirhasan89@gmail.com>
2022-04-23 12:56:43 +0600

    Pascals triangle : solution

--- src/main/kotlin/leetcode/PascalsTriangle.kt
+++ src/main/kotlin/leetcode/PascalsTriangle.kt
@@ -13,29 +13,25 @@ fun generate(numRows: Int): List<List<Int>> {
     val pascalsList = arrayListOf<List<Int>>()
     for (i in 0 until numRows) {
         val list = arrayListOf<Int>()
-        // If it's the first item in the list then it can only have one in it
-        if (i == 0) {
-            list.add(1)
-        } else {
-            // This loops needs to run from 0 to at least previous list's last index
-            for (j in 0 until i + 1) {
-                // From second row of pascals triangle, it's first and last element in the list is 1
-                // Other elements can be found from the previous rows two adjacent items sum
-                if (j == 0 || j == i) {
-                    list.add(1)
-                } else {
-                    val item = tempList[j - 1] + tempList[j]
-                    list.add(item)
-                }
+        // This loops needs to run from 0 to at least previous list's last index
+        for (j in 0 until i + 1) {
+            // From second row of pascals triangle, it's first and last element in the list is 1
+            // Other elements can be found from the previous rows two adjacent items sum
+            if (j == 0 || j == i) {
+                list.add(1)
+            } else {
+                val item = tempList[j - 1] + tempList[j]
+                list.add(item)
             }
         }
+        // Keep the last generated row to calculate the next row's items
         tempList = list
-        pascalsList.add(list)
+        pascalsList.add(list) // Add row to the triangle
     }
     return pascalsList
 }
 
 fun main() {
-    val res = generate(5)
+    val res = generate(6)
     println(res)
 }
