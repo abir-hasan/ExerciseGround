220f36cf5502a47e28204debb0e9cc49d5081a17
Abir Hasan <abirhasan89@gmail.com>
2022-04-26 23:40:45 +0600

    Linked List Cycle : Solution

--- src/main/kotlin/leetcode/linkedList/LinkedListCycle.kt
+++ src/main/kotlin/leetcode/linkedList/LinkedListCycle.kt
@@ -22,3 +22,32 @@ fun hasCycle(head: ListNode?): Boolean {
     }
     return false
 }
+
+/**
+ * Time Complexity : O(N)
+ * Space Complexity : O(1) // No additional Storage needed for the below solution
+ * Floyd's Cycle detection algorithm :
+ * It's a classic algo for detecting cycles in a linked list.
+ * We use two pointers to traverse the list: The first one is moving one node at the time and
+ * the second two nodes at the time. If there is a cycle,
+ * sooner or later pointers will meet, and we return true.
+ * If the fast pointer reached the end of the list, that means there is no cycle, and we can return false.
+ */
+fun hasCycleV2(head: ListNode?): Boolean {
+    if(head?.next == null)
+        return false
+    // Fast and Slow Pointers
+    var slow : ListNode? = head
+    var fast : ListNode? = head
+
+    // Traverse the list to see if there is any cycle
+    while(fast?.next != null) {
+        slow = slow?.next           // move slow pointer by 1 position
+        fast = fast.next?.next      // move slow pointer by 2 position
+        println("slow: ${slow?.`val`} fast: ${fast?.`val`}")
+        // Cycle found
+        if(slow == fast)
+            return true
+    }
+    return false
+}
